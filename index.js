0b62b9e872a6de1be48cb4986c79c2ea16802a79
Andrew Taber <andrew.e.taber@gmail.com>
2017-06-23 12:24:53 -0700

    Remove debugging output.

--- index.js
+++ index.js
@@ -10,15 +10,17 @@ module.exports = function(cells, positions) {
     var s2 = vec3.subtract([], positions[cell[2]], positions[cell[0]]);
     var wnormal = vec3.scale([], vec3.cross([], s1, s2), 0.5);
     area += vec3.length(wnormal);
+
     var centroid = cell.map(function(index, i) {
       return positions[index];
     }).reduce(function(a, b) {
       return vec3.add([], a, b);
     }, [0, 0, 0])
-    console.log(centroid)
+
     vec3.scale(centroid, centroid, 1 / 3.0);
     centroid[0] = 0;
     centroid[1] = 0;
+
     volume += vec3.dot(centroid, wnormal);
   }
 
